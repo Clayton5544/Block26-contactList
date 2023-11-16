7e0e9900f2d8d07e6077ca041d01eb3c8cef29d2
Clayton5544 <142748623+Clayton5544@users.noreply.github.com>
2023-11-16 20:00:44 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+//import { useState } from 'react'
+//import React from "react"; 
+import './App.css';
+import ContactList from './ContactList.jsx';
+import React, { useState } from 'react';
+
+
+ const App = () => {
+
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+App()
+
+export default App
+
+
+/*function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <contactList />
+
+    </>
+  );
+}
+
+export default App
+*/
